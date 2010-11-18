b514f805c7e8196d18f1436a08b6c1c3e6198724
Rurik A Primiani <rprimian@cfa.harvard.edu>
2010-11-18 21:32:52 +0000

    Added AList and SKD file parsers and a simple calibration script.
    alist.AList is a container for alist.AScan objects which store
    information from particular scans. When accessing a particular
    attribute of an AList object, such as 'source', it returns a list
    of that attribute for every scan it contains. More documentation to
    come...
    
    parse_skd parses SKD schedule files for scan start times, sources,
    and many other parameters. In particular for the phased array it looks
    for comments containing the current configuration of the system.
    
    The calibrate script uses the above to parses and is also capable
    of parsing SMA Tsys files in order to calibrate the a data set with
    Tsys and then plot and print out a formatted file.
    
    I also removed 'padsCN' as it's unrelated.

---
+++ parse_skd.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+
+import re
+import time
+
+
+TIME_FMT = '%Yy%jd%Hh%Mm%Ss'
+
+
+SCAN_RE = r"""scan\ (?P<name>\w+);\n          # scan specifier
+              \*.+\n\*\ +(?P<pant>[1-8]*)     # get the PA antenna
+              (?P<conf>[dDfFwWaApP]*)         # get the configuration
+              \(x\)(?P<comp>[0-9]).+\n             # get the comp. antenna
+               \ +start=(?P<time>\w+);        # start time
+              \ mode=(?P<mode>\w+);           # freq. def used
+              \ source=(?P<source>.+);\n      # source name
+              (?:\*.+\n)?^.+;\n ^.+;\n        # skips two (or three) lines
+              endscan;$"""
+
+def parse_skd(FILE, debug=False):
+    SCANS = []
+    STR = ''.join(FILE.readlines())
+    ITER = re.finditer(SCAN_RE, STR, re.X | re.M)
+    for match in ITER:
+        SCAN = match.groupdict()
+        SCAN['pant'] = [int(a) for a in SCAN['pant']]
+        SCAN['comp'] = int(SCAN['comp'][0])
+        SCAN['conf'] = list(SCAN['conf'].upper())
+        SCAN['time'] = time.asctime(\
+            time.strptime(SCAN['time'], TIME_FMT))
+        SCANS.append(SCAN)
+        if debug:
+            print "FOUND SCAN", SCAN['name']
+            print "  PA ANT", SCAN['pant']
+            print "  COMP ANT", SCAN['comp']
+            print "  CONFIG", ''.join(SCAN['conf'])
+            print "  USING", SCAN['mode']
+            print "  AT", SCAN['time']
+            print "  ON SOURCE", SCAN['source']
+    if debug:
+        print "TOTAL: %d scans" %len(SCANS)
+    return SCANS
+    
+
+if __name__ == '__main__':
+
+    import sys
+
+    if len(sys.argv) <> 2:
+        raise AttributeError, "No .skd file specified!"
+    elif not sys.argv[1].endswith('.skd'):
+        raise AttributeError, "File given is not .skd!"
+    else:
+        print "PARSING:", sys.argv[1]
+
+    FILE = open(sys.argv[1])
+    scans = parse_skd(FILE, debug=True)
+
