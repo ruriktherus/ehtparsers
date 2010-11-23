6e0b447b5f503e82968f00243dd8fe78447e2824
Rurik A Primiani <rprimian@cfa.harvard.edu>
2010-11-23 17:58:31 -0500

    AbstractList now functions more like an ordered dictionary.
    Overrode dictionary methods that would normally return keys and values
    in an unordered manner in order to sort them by the pivot appropriately.
    The user can now use .iterkeys(), .itervalues(), .iteritems() and their cor-
    responding list apprehensions .keys(), .values(), .items() to get pivot
    sorted versions of the scan objects.
    
    Also overrode the __getattr__ method to allow the user to get scan
    attributes for all scans within the list by simply using <list>.<attr>.
    This will return a(n ordered) list of that attr for all scans. This works
    particularly well when plotting,
    
    i.e. plot(list.datetime, list.amplitude)
    
    and I may consider returning a Numpy array instead of a list in the future.

--- core.py
+++ core.py
@@ -7,7 +7,6 @@ class AbstractRepr:
     def __init__(self, repr_format="<{name}>", **kwargs):
         self.name = self.__class__.__name__
         self.repr_format = repr_format
-        self.__dict__.update(self)
 
     def __repr__(self):
         return self.repr_format.format(**self.__dict__)
@@ -18,6 +17,7 @@ class AbstractScan(AbstractRepr, dict):
     def __init__(self, dict_, **kwargs):
         dict.__init__(self, dict_, **kwargs)
         AbstractRepr.__init__(self, **kwargs)
+        self.__dict__.update(self)
 
     def __eq__(self, other):
         """ inst == other -> bool
@@ -44,6 +44,27 @@ class AbstractList(AbstractRepr, dict):
         if self.__len__() != len(list_):
             raise DataOverriddenError(list_, self)
 
+    def __iter__(self):
+        for key in sorted(dict.keys(self)):
+            yield key
+
+    def __getitem__(self, key):
+        if isinstance(key, int):
+            pivot = self.keys()[key]
+        else:
+            pivot = key
+        return dict.__getitem__(self, pivot)
+
+    def __getattr_iter__(self, attr):
+        for value in self.itervalues():
+            try:
+                yield getattr(value, attr)
+            except AttributeError:
+                raise FieldNotFoundError(value, attr)
+
+    def __getattr__(self, attr):
+        return list(self.__getattr_iter__(attr))
+
     def _scancheck(self, list_):
         """ dict = inst._merge_list(list_)
         This function produces a dictionary where the keys
@@ -61,3 +82,30 @@ class AbstractList(AbstractRepr, dict):
             if entry.pivot != pivot:
                 raise InconsistentPivotError(index, entry, pivot)
             yield entry[pivot], entry
+
+    def iterkeys(self):
+        for key in self.__iter__():
+            yield key
+
+    def itervalues(self):
+        for key in self.__iter__():
+            value = dict.__getitem__(self, key)
+            yield value
+
+    def iteritems(self):
+        for key in self.__iter__():
+            value = dict.__getitem__(self, key)
+            yield key, value
+
+    def keys(self):
+        return list(self.iterkeys())
+
+    def values(self):
+        return list(self.itervalues())
+
+    def items(self):
+        return list(self.iteritems())
+
+    def popitem(self):
+        first = self.__iter__().next()
+        return dict.pop(self, first)
