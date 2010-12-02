35863240f63d8477eb00c71cf4da99435a03c97e
Rurik A Primiani <rprimian@cfa.harvard.edu>
2010-12-02 13:42:15 -0500

    Added SMA Tsys file handlers, SMATsysScan and SMATsysList.
    They are subclassed from AbstractScan and AbstractList respectively.
    Since the SMA Tsys files may have multiple values for a single datetime
    this module currently takes the first given value and ignores the others.
    However it does spit out a warning to the logging system when this happens.
    Not sure of the best way to handle this.

---
+++ vlbidata/tsys/sma.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+
+from re import compile
+from datetime import datetime
+from warnings import warn_explicit
+
+from vlbidata.core import AbstractScan, AbstractList
+from vlbidata.errors import ConflictingValuesError
+
+
+__all__ = [
+    'SMATsysScan',
+    'SMATsysList',
+    ]
+
+
+ACC_RE = compile('#\s.+ACC(?P<ant>[0-9]{1,2})')
+
+
+class SMATsysScan(AbstractScan):
+
+    def __init__(self, dict_):
+        self.repr_format = "< {name} @ {datetime} >"
+        AbstractScan.__init__(self, dict_, pivot='datetime', repr_format=self.repr_format)
+
+    def _merge_scans(self, other):
+        return SMATsysScan(dict(self, **other))
+
+    def merge(self, other):
+        try:
+            return AbstractScan.merge(self, other)
+        except ConflictingValuesError:
+            self.logger.warning('** attempt to merge duplicate Tsys value!')
+            return AbstractScan.merge(self, self)
+
+
+class SMATsysList(AbstractList):
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.repr_format = "** {name} of {0.length} scans **"
+        AbstractList.__init__(self, self._parse_tsys(self.filename), merge=True,
+                              repr_format=self.repr_format)
+
+    def _parse_tsys(self, filename):
+        with open(filename, 'r') as file_:
+            for line in file_:
+                if line.startswith('#'):
+                    match = ACC_RE.match(line)
+                    if match:
+                        antenna = match.groupdict()['ant']
+                else:
+                    columns = line.split()
+                    if columns:
+                        tsys = float(columns[-1])
+                        dt = datetime.utcfromtimestamp(int(columns[0]))
+                        yield SMATsysScan({'datetime': dt, 'tsys_sma%s'%antenna: tsys})
