8b2d061447693ca84cc16356d4bd286ef504d7f0
Rurik A Primiani <ruriktherus@gmail.com>
2010-12-03 10:26:33 -0500

    Added a print_table function to the tools module.
    This prints comma separted values files using a supplied AbstractList
    derived object and a Python list of requested fields for each scan.

--- vlbidata/tools.py
+++ vlbidata/tools.py
@@ -1,4 +1,19 @@
 
+from csv import DictWriter
 
-def filter(list_, **conditions):
-    return list_.__filter__(**conditions)
+
+__all__ = [
+    'filter',
+    'print_table',
+]
+
+
+def filter(list_, expression=None, **conditions):
+    return list_.__filter__(expression=expression, **conditions)
+
+
+def print_table(list_, output_filename, keys=None, **kwargs):
+    if keys==None:
+        keys = list_[0].iterkeys()
+    writer = DictWriter(open(output_filename, 'w'), keys, extrasaction='ignore', **kwargs)
+    writer.writerows(list_.itervalues())
