e3fc05a69f8baf0ab04fc8b74be5b599016d5372
Rurik A Primiani <rprimian@cfa.harvard.edu>
2010-12-03 20:07:11 -0500

    Added some functions to the tools module for doing flux calibration using Tsys.

--- vlbidata/tools.py
+++ vlbidata/tools.py
@@ -2,6 +2,8 @@
 import sys
 import logging
 import os.path as path
+
+from math import sqrt
 from csv import DictWriter
 from datetime import datetime
 
@@ -12,6 +14,8 @@ __all__ = [
     'filter',
     'print_table',
     'process_day',
+    'calc_phringes_sefd',
+    'calc_calibrated_flux',
 ]
 
 
@@ -35,9 +39,39 @@ def process_day(day, baseline, year=datetime.now().year, rootdir='/data'):
     sys.stdout.write('done!\r\nMerging into single AList (will take even longer)... ')
     sys.stdout.flush()
     final = all + skd + sma + smto
+    sys.stdout.write('done!\r\nCalibrating local amplitudes by system temperatures...')
+    sys.stdout.flush()
+    calc_calibrated_flux(final)
     sys.stdout.write('done!\r\n')
     sys.stdout.flush()
     return final
+
+
+def calc_phringes_sefd(list_):
+    for scan in list_.itervalues():
+        sma_dish_gain = 130. #Jy/K
+        sefd = 1./sum(1./(2*sma_dish_gain*scan['tsys_sma{0}'.format(pant)]) for pant in scan.pants)
+        scan['phringes_sefd'] = sefd
+
+
+def calc_calibrated_flux(list_):
+    calc_phringes_sefd(list_)
+    for scan in list_.itervalues():
+        sma_dish_gain = 130. #Jy/K
+        smto_dish_gain = 50. #Jy/K
+        phringes_sefd = scan['phringes_sefd']
+        comp_sefd = 2*sma_dish_gain*scan['tsys_sma{0}'.format(scan.comp)]
+        smto_sefd = 2*smto_dish_gain*int(scan['tsys_smto2'])
+        if scan.baseline == 'PZ':
+            hawaii_sefd = phringes_sefd
+            other_sefd = comp_sefd
+        elif scan.baseline == 'SZ':
+            hawaii_sefd = comp_sefd
+            other_sefd = smto_sefd
+        elif scan.baseline == 'SP':
+            hawaii_sefd = phringes_sefd
+            other_sefd = smto_sefd
+        scan['cal_flux'] = scan.amp * sqrt(other_sefd*hawaii_sefd) / 10000.
 
 
 def print_table(list_, output_filename, keys=None, **kwargs):
